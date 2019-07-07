83f9d3c3b9da96820a8afcdda5c03ac2a5bbc61a
vhborges <viniciusborges22@gmail.com>
2019-07-07 16:45:33 -0300

    Add neovim support

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s -f `pwd`/.vimrc ~/.vimrc
+ln -s -f `pwd`/init.vim ~/.config/nvim/init.vim
+ln -s -f `pwd`/coc-settings.json ~/.config/nvim/coc-settings.json
 vim +PluginInstall +qall
