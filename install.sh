1d59dee0e9b4f42e92a232cfb5ef4054e5e8bd7e
vhborges <viniciusborges22@gmail.com>
2019-08-19 00:01:41 -0300

    Prioritize nvim over vim

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s -f `pwd`/.vimrc ~/.vimrc
-if [ -x "$(command -v vim)" ]; then
-  vim +PlugInstall +qall > /dev/null
-elif [ -x "$(command -v nvim)" ]; then
+if [ -x "$(command -v nvim)" ]; then
   mkdir -p ~/.config/nvim/
   ln -s -f `pwd`/init.vim ~/.config/nvim/init.vim
   ln -s -f `pwd`/coc-settings.json ~/.config/nvim/coc-settings.json
   nvim +PlugInstall +qall > /dev/null
+elif [ -x "$(command -v vim)" ]; then
+  vim +PlugInstall +qall > /dev/null
 else
   echo 'Error: neither vim nor neovim is installed' >&2
 fi
