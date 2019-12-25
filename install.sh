1843b2de704f12ddedb098526207970238189c02
vhborges <viniciusborges22@gmail.com>
2019-12-25 16:59:58 -0300

    Add comments to install script

--- install.sh
+++ install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+# Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -s -f `pwd`/.vimrc ~/.vimrc
+
+# Symbolic link .vimrc to home folder
+ln -sf `pwd`/.vimrc ~/.vimrc
+
+# Editor specific instalation (checks for neovim or vim)
 if [ -x "$(command -v nvim)" ]; then
   mkdir -p ~/.config/nvim/
-  ln -s -f `pwd`/init.vim ~/.config/nvim/init.vim
-  ln -s -f `pwd`/coc-settings.json ~/.config/nvim/coc-settings.json
+  ln -sf `pwd`/init.vim ~/.config/nvim/init.vim
+  ln -sf `pwd`/coc-settings.json ~/.config/nvim/coc-settings.json
   nvim +PlugInstall +qall > /dev/null
 elif [ -x "$(command -v vim)" ]; then
   vim +PlugInstall +qall > /dev/null
 else
-  echo 'Error: neither vim nor neovim is installed' >&2
+  echo 'Error: neither vim nor neovim was found' >&2
 fi
