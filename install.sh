8efbf75931b45dd306cdea92fa324128e2bbfe78
Vinicius Borges <viniciusborges22@gmail.com>
2020-01-19 11:41:19 -0300

    Add error checking to install script

--- install.sh
+++ install.sh
@@ -2,19 +2,32 @@
 
 # Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Symbolic link .vimrc to home folder
-ln -sf `pwd`/.vimrc ~/.vimrc
+echo "Installing '.vimrc'..."
+if ! (ln -sf `pwd`/.vimrc ~/.vimrc); then
+	exit 1
 
 # Editor specific instalation (checks for neovim or vim)
 if [ -x "$(command -v nvim)" ]; then
-  mkdir -p ~/.config/nvim/
-  ln -sf `pwd`/init.vim ~/.config/nvim/init.vim
-  ln -sf `pwd`/coc-settings.json ~/.config/nvim/coc-settings.json
-  nvim +PlugInstall +qall > /dev/null
+	echo "Found neovim.\nInstalling configurations..."
+	if ! (mkdir -p ~/.config/nvim/ &&
+		ln -sf `pwd`/init.vim ~/.config/nvim/init.vim &&
+		ln -sf `pwd`/coc-settings.json ~/.config/nvim/coc-settings.json)
+	then
+		exit 1
+	echo "Installing plugins..."
+	if ! (nvim +PlugInstall +qall > /dev/null); then
+		echo "Error while installing plugins. Please try again." 1>&2
+		exit 1
 elif [ -x "$(command -v vim)" ]; then
-  vim +PlugInstall +qall > /dev/null
+	echo "Found vim.\nInstalling plugins..."
+	if ! (vim +PlugInstall +qall > /dev/null); then
+		echo "Error while installing plugins. Please try again." 1>&2
+		exit 1
 else
-  echo 'Error: neither vim nor neovim was found' >&2
+	echo 'Error: neither vim nor neovim was found' 1>&2
+	exit 1
 fi
+echo "Done!"
