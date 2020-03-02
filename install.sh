66f2e703812d9e054ead8a817088acddc146bfff
Vinicius Borges <viniciusborges22@gmail.com>
2020-03-02 13:00:03 -0300

    Fix installation script

--- install.sh
+++ install.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
 
 # Install vim-plug
+echo "Installing vim-plug..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Symbolic link .vimrc to home folder
 echo "Installing '.vimrc'..."
 if ! (ln -sf `pwd`/.vimrc ~/.vimrc); then
+	echo "Linkage error. Exiting..." 1>&2
 	exit 1
+fi
 
 # Editor specific instalation (checks for neovim or vim)
 if [ -x "$(command -v nvim)" ]; then
-	echo "Found neovim.\nInstalling configurations..."
+	echo "Found neovim. Installing configurations..."
 	if ! (mkdir -p ~/.config/nvim/ &&
 		ln -sf `pwd`/init.vim ~/.config/nvim/init.vim &&
 		ln -sf `pwd`/coc-settings.json ~/.config/nvim/coc-settings.json)
-	then
-		exit 1
+		then
+			echo "Linkage error. Exiting..." 1>&2
+			exit 1
+	fi
 	echo "Installing plugins..."
 	if ! (nvim +PlugInstall +qall > /dev/null); then
 		echo "Error while installing plugins. Please try again." 1>&2
 		exit 1
+	fi
 elif [ -x "$(command -v vim)" ]; then
-	echo "Found vim.\nInstalling plugins..."
+	echo "Found vim. Installing plugins..."
 	if ! (vim +PlugInstall +qall > /dev/null); then
 		echo "Error while installing plugins. Please try again." 1>&2
 		exit 1
+	fi
 else
-	echo 'Error: neither vim nor neovim was found' 1>&2
+	echo "Error: neither vim nor neovim was found" 1>&2
 	exit 1
 fi
+
 echo "Done!"
